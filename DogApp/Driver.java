7d89b01760373f8f546491025fca867219a04763
Michael Ross <mrosstech@gmail.com>
2020-11-21 22:23:26 -0500

    Final push

--- DogApp/Driver.java
+++ DogApp/Driver.java
@@ -1,8 +1,8 @@
 
 public class Driver {
 	
-	public static void main() {
-		Corgi myCorgi = new Corgi("Pembroke Welsh", "Corgi", "Java", 38, 5);
+	public static void main(String[] args) {
+		Corgi myCorgi = new Corgi("cattle herding", "Pembroke Welsh Corgi", "Java", 38, 5);
 		myCorgi.setTopTrick("ringing the bell to go outside");
 		System.out.print(myCorgi.toString());
 		
